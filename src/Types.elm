8c36cc3ff9c87f2c483442c8b2d8bc7dd35cbdb3
Kyrylo Yakymenko <yakimych@gmail.com>
2020-07-25 10:50:57 +0200

    Implement types and Piece sets

---
+++ src/Types.elm
@@ -0,0 +1,96 @@
+module Types exposing (..)
+
+import Dict exposing (Dict)
+import Set exposing (Set)
+
+
+type alias BoardMap =
+    Dict ( Int, Int ) PieceShape
+
+
+type alias PieceSet =
+    Set ( Int, Int )
+
+
+type PieceShape
+    = T
+    | S
+    | Z
+    | I
+    | O
+    | L
+    | J
+
+
+type Orientation
+    = Up
+    | Left
+    | Down
+    | Right
+
+
+type BoardTile
+    = OccupiedBy PieceShape
+    | Boundary
+
+
+
+-- PieceI
+
+
+getMatrixI : Orientation -> PieceSet
+getMatrixI orientation =
+    let
+        upDownMatrix =
+            Set.fromList [ ( 2, 0 ), ( 2, 1 ), ( 2, 2 ), ( 2, 3 ) ]
+
+        leftRightMatrix =
+            Set.fromList [ ( 0, 2 ), ( 1, 2 ), ( 2, 2 ), ( 3, 2 ) ]
+    in
+    case orientation of
+        Up ->
+            upDownMatrix
+
+        Down ->
+            upDownMatrix
+
+        Left ->
+            leftRightMatrix
+
+        Right ->
+            leftRightMatrix
+
+
+getMatrixO : () -> PieceSet
+getMatrixO () =
+    Set.fromList [ ( 1, 1 ), ( 2, 1 ), ( 1, 2 ), ( 2, 2 ) ]
+
+
+getPieceSet : PieceShape -> Orientation -> PieceSet
+getPieceSet pieceShape orientation =
+    case pieceShape of
+        I ->
+            getMatrixI orientation
+
+        O ->
+            getMatrixO ()
+
+        _ ->
+            -- TODO: Implement the rest of the cases
+            getMatrixO ()
+
+
+getNextOrientation : Orientation -> Orientation
+getNextOrientation currentOrientation =
+    case currentOrientation of
+        Up ->
+            Left
+
+        Left ->
+            Down
+
+        Down ->
+            Right
+
+        Right ->
+            Up
